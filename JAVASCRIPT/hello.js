9fd857385fe095ab9e7c0659eff4fcd9b4b1194f
Kartik Vurukonda <kartik96vurukonda@gmail.com>
2022-07-03 19:59:47 +0530

    First commit

---
+++ JAVASCRIPT/hello.js
@@ -0,0 +1,32 @@
+// console.log('Hello World');
+/*
+let x=1,y=3,z=5;
+console.log(x,y,z);
+console.log("I love JS");
+console.log('Hello'+' '+'World');
+
+const name = 'ram';
+const name1 = "hari";
+const result = `The names are ${name} and ${name1}`;
+
+const a="abc"/3;
+console.log(a);
+
+const a=prompt("Enter a number: ");
+if(a>3){
+    console.log("The number is greater than 3");
+}
+console.log("The number is not greater than 3");
+*/
+
+function greet(){
+    console.log("Hello");
+}
+
+greet();
+
+let x=function(num){
+console.log(num*num*num);
+}
+
+x(4);
